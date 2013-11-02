487c3cff15dd9609f6712fccc3bbd74ebc9d16da
Aurélien Matouillot <a.matouillot@gmail.com>
2013-11-02 16:09:47 +0100

    Add travis

--- setup.py
+++ setup.py
@@ -1,6 +1,13 @@
 from setuptools import setup, find_packages
 import sys, os
 
+# Hack to prevent TypeError: 'NoneType' object is not callable error
+# on exit of python setup.py test
+try:
+    import multiprocessing
+except ImportError:
+    pass
+
 version = '0.0'
 
 setup(name='colorterm/',
@@ -19,6 +26,10 @@ setup(name='colorterm/',
       zip_safe=False,
       install_requires=[
           # -*- Extra requirements: -*-
+      ],
+      test_suite='nose.collector',
+      tests_require=[
+          'nose',
       ],
       entry_points="""
       # -*- Entry points: -*-
