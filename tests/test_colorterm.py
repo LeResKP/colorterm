e2fb7a5dfde420b9aa006a8f7c6792f2d5ffbc9a
Aurélien Matouillot <a.matouillot@gmail.com>
2013-11-07 07:55:00 +0100

    Do not add any rendering if the output is not a tty.

--- tests/test_colorterm.py
+++ tests/test_colorterm.py
@@ -1,9 +1,17 @@
 import unittest
+import sys
 from colorterm.colorterm import parse_attr, applycolor, formatter
 from colorterm import colorterm
 
 
 class TestColorTerm(unittest.TestCase):
+
+    def setUp(self):
+        self.old_isatty = sys.stdout.isatty
+        sys.stdout.isatty = lambda: True
+
+    def tearDown(self):
+        sys.stdout.isatty = self.old_isatty
 
     def test_formatter(self):
         res = formatter('{red}{underline}Hello{/underline} world')
@@ -16,6 +24,12 @@ class TestColorTerm(unittest.TestCase):
 
         res = formatter('{on_green}{red}Hello{/red}{/on_green} world')
         expected = '\x1b[42m\x1b[31mHello\x1b[39m\x1b[49m world\x1b[0m'
+        self.assertEqual(res, expected)
+
+    def test_formatter_no_tty(self):
+        sys.stdout.isatty = self.old_isatty
+        res = formatter('{red}{underline}Hello{/underline} world')
+        expected = 'Hello world'
         self.assertEqual(res, expected)
 
     def test_parse_attr(self):
@@ -90,6 +104,12 @@ class TestColorTerm(unittest.TestCase):
 
         res = applycolor('Hello world', fgcolor=31)
         expected = '\x1b[31mHello world\x1b[0m'
+        self.assertEqual(res, expected)
+
+    def test_applycolor_no_tty(self):
+        sys.stdout.isatty = self.old_isatty
+        res = applycolor('Hello world', formatting=4, bgcolor=42, fgcolor=31)
+        expected = 'Hello world'
         self.assertEqual(res, expected)
 
     def test_colorterm(self):
